7463ba0426502172843d1d902a498512202e2e43
iam.camilomillan <iam.camilomillan@gmail.com>
2025-11-09 19:02:41 -0500

    Basic pages structure.

---
+++ src/app/(private)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <section>
+        <div className="grid gap-8">
+          <span>A very beautiful dashboard page.</span>
+        </div>
+      </section>
+    </main>
+  );
+}
